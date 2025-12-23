7c87fe6468b6be8650de17037be0bb5864900d78
Exceen <github@exceen.net>
2025-12-23 10:39:43 +0100

    added ignoreAuthForHostEndsWith setting for remote

--- src/renderer/store/settings.store.ts
+++ src/renderer/store/settings.store.ts
@@ -593,6 +593,7 @@ const PlaybackSettingsSchema = z.object({
 
 const RemoteSettingsSchema = z.object({
     enabled: z.boolean(),
+    ignoreAuthForHostEndsWith: z.string(),
     password: z.string(),
     port: z.number(),
     username: z.string(),
@@ -1723,6 +1724,7 @@ const initialState: SettingsState = {
     },
     remote: {
         enabled: false,
+        ignoreAuthForHostEndsWith: '',
         password: randomString(8),
         port: 4333,
         username: 'feishin',
