c22a15e221f127973ba0556b53f9ca16384ad182
Exceen <github@exceen.net>
2025-12-27 22:25:07 +0100

    adjusted library-header to be more compact

--- src/renderer/features/artists/routes/album-artist-detail-route.tsx
+++ src/renderer/features/artists/routes/album-artist-detail-route.tsx
@@ -101,7 +101,7 @@ const AlbumArtistDetailRouteContent = () => {
                             </LibraryHeaderBar.Title>
                         </LibraryHeaderBar>
                     ),
-                    offset: 200,
+                    offset: 120,
                     target: headerRef,
                 }}
                 ref={scrollAreaRef}
